3d64189447de0ee49357f9b846a2fd6cd2a411ab
Florence To <florenceto@live.co.uk>
2026-08-02 19:15:43 +0100

    Update

--- src/pages/BirdMapPage.jsx
+++ src/pages/BirdMapPage.jsx
@@ -103,7 +103,7 @@ export default function BirdMapPage() {
           color: #222;
         }
         .map-container {
-          margin-top: 2rem;
+          margin-top: 1rem;
           border: 1px solid #9afc97;
           padding: 1rem;
           background: rgba(0, 0, 0, 0.2);
